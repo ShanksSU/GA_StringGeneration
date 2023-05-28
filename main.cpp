6073aad4cb74a86e7962548f0efb946a883f3100
ShanksSU <87661487+ShanksSU@users.noreply.github.com>
2023-05-28 21:43:02 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include "lib/GA.h"
-using namespace std;
 
 int main() {
-    string targetStr = "Hello World!";
-    printf_s("Result string¡G%s\n", entry(targetStr).c_str());
+    std::string targetStr = "Hello World!";
+    printf_s("Result string：%s\n", entry(targetStr).c_str());
     return 0;
 }
